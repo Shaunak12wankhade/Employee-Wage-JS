d8c9b0e619361f9f7fa4302a74cf55182e1f0b1f
unknown <shaunak12wankhade@ggmail.com>
2021-11-21 11:38:59 +0530

    [Shaunak] Added : Daily wages for part time, full time & absent employees

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,7 +2,6 @@
 
 //Uc1 checking employee attendance 
 const IS_ABSENT=0;
-
 //Using random 
 let empCheck=Math.floor(Math.random()*10)%2;
 
@@ -14,3 +13,30 @@ else
 {
    console.log("Employee is present");
 }
+
+
+//UC2
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const WAGE_PER_HR=20;
+
+let empHrs=0;
+let empCheck1=Math.floor(Math.random()*10)%3;
+switch(empCheck1)
+{
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        console.log("UC2-->Part time employee  "+empHrs);
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        console.log("UC2-->Full time employee "+empHrs);
+        break;
+    default:
+        empHrs=0;
+        console.log("UC2-->Employee is absent "+empHrs);
+}
+let empWage=empHrs*WAGE_PER_HR;
+console.log("Daily Wage : "+empWage);
