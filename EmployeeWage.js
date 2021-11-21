70d42f5a549026d2163f185a68c4051af9dd8d85
unknown <shaunak12wankhade@ggmail.com>
2021-11-21 11:28:39 +0530

    [Shaunak] Added : Employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,16 @@
-Welcome to EmployeeWage JS
+//Welcome to EmployeeWage JS
+
+//Uc1 checking employee attendance 
+const IS_ABSENT=0;
+
+//Using random 
+let empCheck=Math.floor(Math.random()*10)%2;
+
+if(empCheck==IS_ABSENT)
+{
+  console.log("Employee is absent");
+}
+else
+{
+   console.log("Employee is present");
+}
